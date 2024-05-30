cf8a437824c187a30325cd061c86b110e511522e
khadijara <khadiaaa@users.noreply.github.com>
2024-05-30 02:04:26 +0200

    Guia Nutricional pagina

--- frontend/src/components/Nutrtional-guide/guide.js
+++ frontend/src/components/Nutrtional-guide/guide.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './guide.css';
 import pyramidImage from '../../assets/nutritional.jpg';
 import avocadoImage from '../../assets/avocado.jpeg';
@@ -20,66 +20,113 @@ import wholeGrainImage from '../../assets/wholegrain.jpeg';
 import Navbar from '../Navbar/Navbar';
 
 const Guide = () => {
-    return (
-        <> 
-        < Navbar />
+  const [modalContent, setModalContent] = useState(null);
 
-        <div className="guide">
-            <header className="header">
-                <h1>Guía Nutricional</h1>
-            </header>
-            <main className="main-content">
-                <section className="intro">
-                    <p>En nuestro apartado de 'Guía Nutricional', te ofrecemos recursos completos y prácticos para ayudarte a alimentarte de manera saludable y equilibrada. Sabemos que la nutrición es fundamental para alcanzar tus metas fitness y mantener un estilo de vida saludable en general.</p>
-                    <p>Nuestra guía está diseñada para proporcionarte información útil sobre cómo planificar comidas saludables, equilibrar tus nutrientes, y hacer elecciones inteligentes en tu dieta diaria. Desde consejos sobre cómo estructurar tus comidas hasta recetas deliciosas y nutritivas, estamos aquí para apoyarte en cada paso del camino.</p>
-                    <p>Ya sea que estés buscando perder peso, ganar masa muscular o simplemente mejorar tu salud en general, nuestra guía nutricional te ayudará a tomar decisiones informadas y adoptar hábitos alimenticios que te lleven más cerca de tus objetivos. ¡Explora nuestros recursos y haz de la nutrición un pilar fundamental de tu bienestar!</p>
-                </section>
-                <section className="content">
-                    <div className="content-block">
-                        <h2>La pirámide alimenticia</h2>
-                        <p>La pirámide alimenticia es una guía visual que muestra qué alimentos y en qué cantidad se deben consumir para tener una dieta equilibrada.</p>
-                        <img src={pyramidImage} alt="Pirámide alimenticia" className="content-image"/>
-                    </div>
-                    <div className="content-block">
-                        <h2>Grasas saludables</h2>
-                        <ul>
-                            <li><img src={avocadoImage} alt="Aguacates" className="icon"/> <strong>Aguacates:</strong> Son ricos en grasas monoinsaturadas, que son beneficiosas para la salud del corazón.</li>
-                            <li><img src={oliveOilImage} alt="Aceite de oliva" className="icon"/> <strong>Aceite de oliva virgen extra:</strong> Contiene principalmente grasas monoinsaturadas y es una opción saludable para cocinar y aderezar ensaladas.</li>
-                            <li><img src={nutsImage} alt="Frutos secos" className="icon"/> <strong>Frutos secos (nueces, almendras, avellanas, etc.):</strong> Son una fuente de grasas saludables, proteínas y fibra.</li>
-                            <li><img src={seedsImage} alt="Semillas" className="icon"/> <strong>Semillas (chia, lino, calabaza, girasol, etc.):</strong> Son ricas en ácidos grasos omega-3 y omega-6, que son importantes para la salud cardiovascular y cerebral.</li>
-                            <li><img src={fishImage} alt="Pescado graso" className="icon"/> <strong>Pescado graso (salmón, sardinas, trucha, etc.):</strong> Contienen ácidos grasos omega-3, que tienen numerosos beneficios para la salud, incluyendo la reducción del riesgo de enfermedades cardíacas.</li>
-                            <li><img src={olivesImage} alt="Aceitunas" className="icon"/> <strong>Aceitunas:</strong> Son una buena fuente de grasas saludables, especialmente cuando se consumen en forma de aceite de oliva.</li>
-                        </ul>
-                    </div>
-                    <div className="content-block">
-                        <h2>Fuentes de proteínas</h2>
-                        <ul>
-                            <li><img src={chickenImage} alt="Pollo" className="icon"/> <strong>Pollo:</strong> Es una excelente fuente de proteínas magras y es versátil en la cocina.</li>
-                            <li><img src={fishImage} alt="Pescado" className="icon"/> <strong>Pescado (salmón, atún, trucha, etc.):</strong> Los pescados grasos son ricos en proteínas de alta calidad y ácidos grasos omega-3.</li>
-                            <li><img src={eggsImage} alt="Huevos" className="icon"/> <strong>Huevos:</strong> Son una excelente fuente de proteínas de alta calidad y también contienen otros nutrientes importantes como vitaminas B12 y D.</li>
-                            <li><img src={legumesImage} alt="Legumbres" className="icon"/> <strong>Legumbres (lentejas, garbanzos, frijoles, etc.):</strong> Son una fuente de proteínas vegetales, fibra y carbohidratos complejos.</li>
-                            <li><img src={tofuImage} alt="Tofu y tempeh" className="icon"/> <strong>Tofu y tempeh:</strong> Son productos de soja ricos en proteínas y son opciones populares para dietas vegetarianas y veganas.</li>
-                            <li><img src={dairyImage} alt="Productos lácteos" className="icon"/> <strong>Productos lácteos (yogur griego, queso cottage, leche):</strong> Son buenas fuentes de proteínas de alta calidad, además de contener calcio y otros nutrientes.</li>
-                            <li><img src={meatImage} alt="Carne magra" className="icon"/> <strong>Carne magra (ternera magra, pavo, cerdo magro):</strong> Son fuentes ricas en proteínas y también proporcionan hierro y otros nutrientes.</li>
-                        </ul>
-                    </div>
-                    <div className="content-block">
-                        <h2>Ricos en fibra</h2>
-                        <ul>
-                            <li><img src={fruitsImage} alt="Frutas" className="icon"/> <strong>Frutas:</strong> Manzanas, peras, plátanos, fresas, arándanos, naranjas, etc. Las frutas frescas son una excelente fuente de fibra, especialmente cuando se consumen con la piel.</li>
-                            <li><img src={vegetablesImage} alt="Verduras" className="icon"/> <strong>Verduras:</strong> Brócoli, espinacas, zanahorias, col rizada, coliflor, pimientos, etc. Las verduras son ricas en fibra y nutrientes esenciales para la salud.</li>
-                            <li><img src={legumesImage} alt="Legumbres" className="icon"/> <strong>Legumbres:</strong> Lentejas, garbanzos, frijoles negros, frijoles rojos, guisantes, etc. Las legumbres son una excelente fuente de fibra soluble e insoluble, así como de proteínas.</li>
-                            <li><img src={grainsImage} alt="Cereales integrales" className="icon"/> <strong>Cereales integrales:</strong> Avena, arroz integral, quinoa, cebada, bulgur, trigo sarraceno, etc. Los cereales integrales son ricos en fibra y proporcionan energía duradera.</li>
-                            <li><img src={nutsImage} alt="Frutos secos y semillas" className="icon"/> <strong>Frutos secos y semillas:</strong> Almendras, nueces, semillas de chía, semillas de lino, semillas de calabaza, etc. Los frutos secos y las semillas son ricos en fibra, grasas saludables y otros nutrientes.</li>
-                            <li><img src={wholeGrainImage} alt="Pan integral" className="icon"/> <strong>Pan integral y productos de grano entero:</strong> Pan integral, pasta integral, arroz integral, cereales integrales, etc. Estos productos son una buena fuente de fibra y nutrientes esenciales.</li>
-                        </ul>
-                    </div>
-                </section>
-            </main>
-        </div>
-        </>
-    );
+  const openModal = (content) => {
+    setModalContent(content);
+  };
 
+  const closeModal = () => {
+    setModalContent(null);
+  };
+
+  const items = [
+    { title: 'Aguacates', image: avocadoImage, description: 'Son ricos en grasas monoinsaturadas, que son beneficiosas para la salud del corazón.' },
+    { title: 'Aceite de oliva virgen extra', image: oliveOilImage, description: 'Contiene principalmente grasas monoinsaturadas y es una opción saludable para cocinar y aderezar ensaladas.' },
+    { title: 'Frutos secos', image: nutsImage, description: 'Son una fuente de grasas saludables, proteínas y fibra.' },
+    { title: 'Semillas', image: seedsImage, description: 'Son ricas en ácidos grasos omega-3 y omega-6, que son importantes para la salud cardiovascular y cerebral.' },
+    { title: 'Pescado graso', image: fishImage, description: 'Contienen ácidos grasos omega-3, que tienen numerosos beneficios para la salud, incluyendo la reducción del riesgo de enfermedades cardíacas.' },
+    { title: 'Aceitunas', image: olivesImage, description: 'Son una buena fuente de grasas monoinsaturadas y polifenoles, que tienen propiedades antioxidantes.' },
+    { title: 'Pollo', image: chickenImage, description: 'Es una carne magra y una excelente fuente de proteínas de alta calidad.' },
+    { title: 'Huevos', image: eggsImage, description: 'Son una fuente completa de proteínas y contienen todos los aminoácidos esenciales.' },
+    { title: 'Legumbres', image: legumesImage, description: 'Son una fuente de proteínas vegetales y fibra.' },
+    { title: 'Tofu', image: tofuImage, description: 'Es una excelente fuente de proteínas para vegetarianos y veganos, y es bajo en grasas saturadas.' },
+    { title: 'Productos lácteos', image: dairyImage, description: 'Son fuentes de proteínas de alta calidad, así como de calcio y otros nutrientes esenciales.' },
+    { title: 'Carne de res magra', image: meatImage, description: 'Es rica en proteínas y nutrientes como el hierro y el zinc.' },
+    { title: 'Frutas', image: fruitsImage, description: 'Son una excelente fuente de fibra dietética, vitaminas y antioxidantes.' },
+    { title: 'Verduras', image: vegetablesImage, description: 'Son ricas en fibra, vitaminas y minerales esenciales para la salud.' },
+    { title: 'Granos enteros', image: grainsImage, description: 'Contienen fibra y otros nutrientes beneficiosos para la digestión y la salud en general.' },
+    { title: 'Pan integral', image: wholeGrainImage, description: 'Es una buena fuente de fibra y nutrientes esenciales.' },
+  ];
+
+  return (
+    <>
+      <Navbar />
+      <div className="guide">
+        <header className="header">
+          <h1>Guía Nutricional</h1>
+        </header>
+        <main className="main-content">
+          <div className="contents">
+            <section className="intro">
+              <p>
+                En nuestro apartado de 'Guía Nutricional', te ofrecemos recursos completos y prácticos para ayudarte a alimentarte de manera saludable y equilibrada. Sabemos que la nutrición es fundamental para alcanzar tus metas fitness y mantener un estilo de vida saludable en general.
+              </p>
+              <p>
+                Nuestra guía está diseñada para proporcionarte información útil sobre cómo planificar comidas saludables, equilibrar tus nutrientes, y hacer elecciones inteligentes en tu dieta diaria. Desde consejos sobre cómo estructurar tus comidas hasta recetas deliciosas y nutritivas, estamos aquí para apoyarte en cada paso del camino.
+              </p>
+              <p>
+                Ya sea que estés buscando perder peso, ganar masa muscular o simplemente mejorar tu salud en general, nuestra guía nutricional te ayudará a tomar decisiones informadas y adoptar hábitos alimenticios que te lleven más cerca de tus objetivos. ¡Explora nuestros recursos y haz de la nutrición un pilar fundamental de tu bienestar!
+              </p>
+            </section>
+          </div>
+            <br></br>
+          <section className="section grasas-saludables">
+            <div className="content-block">
+              <h2>Grasas saludables</h2>
+              <ul>
+                {items.slice(0, 6).map((item, index) => (
+                  <li key={index} onClick={() => openModal(item)}>
+                    <img src={item.image} alt={item.title} className="icon" />
+                    <strong>{item.title}:</strong> {item.description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+
+          <section className="section fuentes-proteinas">
+            <div className="content-block">
+              <h2>Fuentes de proteínas</h2>
+              <ul>
+                {items.slice(6, 12).map((item, index) => (
+                  <li key={index} onClick={() => openModal(item)}>
+                    <img src={item.image} alt={item.title} className="icon" />
+                    <strong>{item.title}:</strong> {item.description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+
+          <section className="section ricos-fibra">
+            <div className="content-block">
+              <h2>Ricos en fibra</h2>
+              <ul>
+                {items.slice(12).map((item, index) => (
+                  <li key={index} onClick={() => openModal(item)}>
+                    <img src={item.image} alt={item.title} className="icon" />
+                    <strong>{item.title}:</strong> {item.description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+
+          {modalContent && (
+            <div className="modal show" onClick={closeModal}>
+              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <span className="close" onClick={closeModal}>&times;</span>
+                <img src={modalContent.image} alt={modalContent.title} className="icon" />
+                <h2>{modalContent.title}</h2>
+                <p>{modalContent.description}</p>
+              </div>
+            </div>
+          )}
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Guide;
